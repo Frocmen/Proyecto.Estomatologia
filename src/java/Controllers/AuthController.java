d825e1b5c59c452ea7b9922a06a5c497ed1b825e
Andres <andresvillacortarmeza@gmail.com>
2026-06-04 12:55:38 -0500

    Proyecto de Dental Haelth mejorado

--- src/java/Controllers/AuthController.java
+++ src/java/Controllers/AuthController.java
@@ -34,10 +34,33 @@ public class AuthController extends HttpServlet {
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
+         response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet AuthController</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet AuthController at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
 
-        String action = request.getParameter("action");
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+   
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         String action = request.getParameter("action");
         JsonObject jsonResponse = new JsonObject();
 
         try (PrintWriter out = response.getWriter()) {
@@ -94,20 +117,6 @@ public class AuthController extends HttpServlet {
             jsonResponse.addProperty("message", "Error: " + e.getMessage());
             response.getWriter().print(jsonResponse.toString());
         }
-    }
-
-    
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-   
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
     }
 
     
